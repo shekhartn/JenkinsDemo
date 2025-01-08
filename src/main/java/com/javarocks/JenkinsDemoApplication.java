20a0ecd3fddb83e64f2fa0f70be752cc7b66ed39
KARTHIK T <karthikt@KARTHIKs-MacBook-Air.local>
2025-01-08 10:34:52 +0530

    Added test case

--- src/main/java/com/javarocks/JenkinsDemoApplication.java
+++ src/main/java/com/javarocks/JenkinsDemoApplication.java
@@ -1,21 +1,35 @@
 package com.javarocks;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.GetExchange;
 
+
 @SpringBootApplication
 @RestController
 public class JenkinsDemoApplication {
 
+	public static Logger log= LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	@PostConstruct
+	public void init(){
+		log.info("Project Initiated");
+	}
+
 	public static void main(String[] args) {
+		log.info("Jenkins Project Started....");
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
 
+
 	@GetMapping("/")
 	public String test(){
+		log.info("Jenkins Demo First API....");
 		return "Jenkins Demo";
 	}
 
--- src/test/java/com/javarocks/JenkinsDemoApplicationTests.java
+++ src/test/java/com/javarocks/JenkinsDemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.javarocks;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class JenkinsDemoApplicationTests {
+	public static Logger log= LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		log.info("Test case executing...");
+		assertEquals(true, true);
 	}
 
 }
